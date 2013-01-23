43960d8c4fdb5e06d1b6c0520c01639fcc994cd2
Greg Lee Coleman <gleeco@cloudflare.com>
2013-01-23 09:40:08 -0800

    v.0.5.5 terminate on window.cf_slider_disable

--- public/javascripts/caddi.js
+++ public/javascripts/caddi.js
@@ -32,7 +32,7 @@ CloudFlare.define( 'caddi', [       'caddi/config', 'cloudflare/dom',   'cloudfl
         currTime    = currTs(),
         httpOnly    = parseInt( cfg.http_only, 10 ) || 1,
         sectionId   = ( cfg.text_only ) ? parseInt(cfg.LYRM_id, 10) ||  '3612448' : parseInt(cfg.LYRM_id, 10) || '3612448',
-        V           = cfg.version || '0.5.4',
+        V           = cfg.version || '0.5.5',
         D           = cfg.debug || 1,
         cVal        = '',
 
@@ -96,6 +96,9 @@ CloudFlare.define( 'caddi', [       'caddi/config', 'cloudflare/dom',   'cloudfl
     if ( httpOnly &&  window.location.protocol === 'https:' ){
         terminate++;
         D  &&  console.log( "httpOnly; terminate="+terminate);
+    }
+    if (window.cf_slider_disable ){
+        terminate++;
     }
 
     if(  minRes && viewport ) {
